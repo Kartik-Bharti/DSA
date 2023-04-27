331c093f6e4bbf4c509da058bce130cec092d2f0
Kartik Bharti <cartikofficial@gmail.com>
2023-04-27 17:35:18 +0530

    updated

--- binary_search.c
+++ binary_search.c
@@ -1,47 +1,48 @@
-#include<stdio.h>
-int bs(int arr[],int a,int b, int val)
+#include <stdio.h>
+int bs(int arr[], int a, int b, int val)
 {
-    if(b>=a)
+    if (b >= a)
     {
-        int mid=(a+b)/2;
-        if(arr[mid]==val)
+        int mid = (a + b) / 2;
+        if (arr[mid] == val)
         {
-            return mid+1;
+            return mid + 1;
         }
-        else if(arr[mid]<val)
+        else if (arr[mid] < val)
         {
-            return bs(arr,mid+1,b,val);
+            return bs(arr, mid + 1, b, val);
         }
         else
         {
-            return bs(arr,a,mid-1,val);
+            return bs(arr, a, mid - 1, val);
         }
     }
-    else 
+    else
     {
         return -1;
     }
 }
+
 int main()
 {
-    int arr[]={10,11,12,13,14,15,16,17,18,19,20};
-    int val=17;
-    int n=sizeof(arr)/sizeof(arr[0]);
-    int value= bs(arr,0,n-1,val);
+    int arr[] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    int val = 17;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int value = bs(arr, 0, n - 1, val);
 
     printf("Elements of array are\n");
     for (int i = 0; i < n; i++)
     {
-        printf("%d \n",arr[i]);
+        printf("%d \n", arr[i]);
     }
-    printf("elment to found is =  %d\n",val);
-    if(value==-1)
+    printf("elment to found is =  %d\n", val);
+    if (value == -1)
     {
         printf("element not found\n");
     }
-    else 
+    else
     {
-        printf("element founded on %d place\n",value);
+        printf("element founded on %d place\n", value);
     }
     return 0;
 }
